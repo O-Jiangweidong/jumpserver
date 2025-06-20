0a7f3bd0a5e1a778661f74744a72456de88c151a
jiangweidong <1053570670@qq.com>
2025-06-20 14:51:59 +0800

    perf: 优化认证逻辑

--- apps/custom_files/custom_auth.py
+++ apps/custom_files/custom_auth.py
@@ -4,6 +4,10 @@ import hashlib
 import requests
 
 from django.conf import settings
+from common.utils import get_logger
+
+
+logger = get_logger(__name__)
 
 
 def get_md5(raw):
@@ -13,18 +17,23 @@ def get_md5(raw):
 def authenticate(username, password, **kwargs):
     access_key = os.environ.get('CUSTOM_AUTH_ACCESS_KEY')
     secret_key = os.environ.get('CUSTOM_AUTH_SECRET_KEY')
+    sign = os.environ.get('CUSTOM_AUTH_SIGN')
     url = os.environ.get('CUSTOM_AUTH_URL')
 
     headers = {
-        'otp-sign': get_md5(f'{access_key}{secret_key}'),
-        'otp-ak': access_key
+        'otp-sign': sign, 'otp-ak': access_key, 'opt-sk': secret_key
     }
-    data = {'token': password, 'ucid': username}
-    requests.post(url, json=data, headers=headers).raise_for_status()
-    email_suffix = settings.EMAIL_SUFFIX or 'jumpserver.com'
+    data = {'token': password, 'account': username}
+    try:
+        resp = requests.post(url, json=data, headers=headers)
+        resp.raise_for_status()
+        status = resp.json().get('data', {}).get('status', False)
+        if status is False:
+            logger.warning("PinAuthBackend failed, status error")
+            return None
+    except Exception as e:
+        logger.warning("PinAuthBackend failed, except happened:%s" % e)
+        return None
     return {
-        'name': username,
-        'username': username,
-        'email': f'{username}@{email_suffix}',
-        'is_active': True
+        'name': username, 'username': username, 'is_active': True
     }
