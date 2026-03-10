e02c33a995aa075a306f73080a6eb7204f0b10e5
jiangweidong <1053570670@qq.com>
2026-03-10 17:55:51 +0800

    tasks

--- apps/tickets/handlers/command_confirm.py
+++ apps/tickets/handlers/command_confirm.py
@@ -1,23 +1,12 @@
-import requests
-
-from celery import shared_task
 from django.conf import settings
 
 from tickets.models import ApplyCommandTicket
 from common.utils import get_logger
 from .base import BaseHandler
+from ..tasks import send_message_to_external
 
 
 logger = get_logger(__name__)
-
-
-@shared_task(verbose_name='Send command review ticket message')
-def send_message_to_external(url, data):
-    try:
-        resp = requests.post(url, json=data)
-        resp.raise_for_status()
-    except Exception as error:
-        logger.error(f'Error sending command review ticket message: {error}')
 
 
 class Handler(BaseHandler):
